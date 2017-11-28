60e7971b60e039d1df8c321de7dd72148f6d4a67
hoa.minho <hominhhoa39@gmail.com>
2017-11-28 16:07:24 +0900

    Add toogle

--- js/dict.js
+++ js/dict.js
@@ -78,4 +78,13 @@ $(function() {
         });
         
     });
+    
+    $(".header").click(function () {
+        $header = $(this);
+        //getting the next element
+        $content = $header.next();
+        //open up the content needed - toggle the slide- if visible, slide up, if not slidedown.
+        $content.slideToggle(500, function () {
+        });
+    });
 });
